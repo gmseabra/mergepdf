75ffd20237546433c3c14c1258fe4e82f9a3d0d1
Gustavo Seabra <42077000+gmseabra@users.noreply.github.com>
2020-04-22 16:57:56 -0400

    The script to merge PDF files

---
+++ merge_pdfs.py
@@ -0,0 +1,23 @@
+# pdf_merging.py
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import sys
+
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    # Write out the merged PDF
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+if __name__ == '__main__':
+    paths = sys.argv[1:]
+    merge_pdfs(paths, output='Article_with_SI.pdf')
+
+
